57d15be846de85a49d3b0ff7d06fcad9d36537d6
zacmos <zac@reshuffle.com>
2020-11-27 16:25:19 +1300

    .

--- README.md
+++ README.md
@@ -1,52 +1,176 @@
-## BEGIN - TO DELETE TILL END
+# reshuffle-toggl-connector
+[Code](https://github.com/reshufflehq/reshuffle-toggl-connector) |  [npm](https://www.npmjs.com/package/reshuffle-toggl-connector) | [Code sample](https://github.com/reshufflehq/reshuffle/tree/master/examples/toggl)
 
-THIS IS A TEMPLATE REPO FOR NEW RESHUFFLE CONNECTORS
-1. Create a new connector repo from this template using this link https://github.com/reshufflehq/reshuffle-template-connector/generate
-2. Clone the repo locally
-3. Rename all occurrences of _CONNECTOR_NAME_
-4. `npm install`
-5. `npm run build:watch`
-6. Implement your events/actions in `src/index.ts`
-7. `npm run lint`
-8. Push your code
-9. Go to https://app.circleci.com/projects/project-dashboard/github/reshufflehq/
-    a. You should see your new connector repo
-    b. click on `Set Up Project` for the repo
-    c. click on `Use Existing Config`
-    d. click on `Start Building`
+`npm install reshuffle-toggl-connector`
 
-10. If circle CI checks are all green, you are all set!
+This connector uses [Official Node Toggl Client](https://www.npmjs.com/package/toggl-api) package.
 
-// Keep documentation template below
+### Reshuffle Toggl Connector
 
-## END
 
-# reshuffle-_CONNECTOR_NAME_-connector
+_ES6 import_: `import { TogglConnector } from 'reshuffle-toggl-connector'` 
 
-### Reshuffle _CONNECTOR_NAME_ Connector
+This is a [Reshuffle](https://dev.reshuffle.com) connector that provides an Interface to the Toggl Client.
 
-This connector provides <description>.
+You need to get your Toggl API Token from your account. 
+See more details [here](https://github.com/toggl/toggl_api_docs#api-token)
 
-#### Configuration Options:
-```typescript
-interface _CONNECTOR_NAME_ConnectorConfigOptions {
-  foo: string // foo description
-  bar?: number // bar description
-}
+The following example creates an API endpoint to list all entries in Toggl between from/to dates:
+
+```js
+const { HttpConnector, Reshuffle } = require('reshuffle')
+const { TogglConnector } = require('reshuffle-toggle-connectors')
+
+const app = new Reshuffle()
+const togglConnector = new TogglConnector(app, { token: 'YOUR_TOGGL_TOKEN' })
+const httpConnector = new HttpConnector(app)
+
+httpConnector.on({ method: 'GET', path: '/list' }, async (event) => {
+  const keys = await togglConnector.getTimeEntries('2020-11-01T09:00:00.000Z', '2020-11-05T17:00:00.000Z')
+  event.res.json(keys)
+})
+
+app.start(8000)
 ```
+
+#### Table of Contents
+
+[Configuration](#configuration) Configuration options
+
+_Connector events_:
+
+[TimeTrackerInitialized](#TimeTrackerInitialized) TimeTracker Initialized
+
+[TimeEntryModified](#TimeEntryModified) TimeTracker Modified
+
+
+_Connector actions_:
+
+[getTimeEntries](#getTimeEntries) Get TimeEntries objects
+
+
+_SDK_:
+
+[sdk](#sdk) Get direct SDK access
+
+##### <a name="configuration"></a>Configuration options
+
+```typescript
+const app = new Reshuffle()
+const togglConnector = new TogglConnector(app, {
+  token: string
+})
+```
+
 
 #### Connector events
 
-##### event1 description
-The connector fires this event when ...
+##### <a name="TimeTrackerInitialized"></a>TimeTracker Initialized event
 
-##### event2 description
-The connector fires this event when ...
+_Example:_
+
+```js
+async (objects) => {
+  console.log(objects)
+}
+```
+
+This event is fired when the connector starts tracking a specific Toggl
+Timer board. More technically, it is fired when the connector first reads
+the content of an Toggl board and does not have a previous record of its
+object in its internal database.
+
+##### <a name="TimeEntryModified"></a>TimeEntry Modified event
+
+_Event parameters:_
+
+```
+
+```
+
+_Handler inputs:_
+
+```
+timeEntry: TimeEntry - TimeEntry info
+```
+
+_Example:_
+
+```js
+async (timeEntry) => {
+  console.log('TimeEntry Updated:')
+  console.log('  Id:', timeEntry.id)
+  console.log('  Start:', timeEntry.start)
+  console.log('  Stop:', timeEntry.stop)
+  console.log('  Description:', timeEntry.description)
+}
+```
+
+This event is triggered once whenever the content of a TimeEntry in the
+Toggl board is modified.
+
+The `timeEntry` info argument has the following format:
+
+```ts
+{
+  id: number,
+  wid: null | number,
+  pid: null | number,
+  billable: boolean,
+  start: string,
+  stop: string,
+  duration: number,
+  description: string,
+  tags: Array<string>,
+  at: string
+}
+```
+
+
 
 #### Connector actions
 
-##### action1
-The connector provides action1 which ...
+##### <a name="getTimeEntries"></a>Get TimeEntries action
 
-##### action2
-The connector provides action2 which ...
+_Definition:_
+
+```ts
+( startDate?: string | number | Date | Moment, 
+  endDate?: string | number | Date | Moment
+) => object[]
+```
+If start_date and end_date are not specified, time entries started during the last 9 days are returned. 
+The limit of returned time entries is 1000.
+
+_Usage:_
+
+```js
+const timeEntries = await togglConnector.getTimeEntries('2021-01-01T09:00:00.000Z', '2021-01-05T17:00:00.000Z')
+```
+
+Get a list of
+[TimeEntries](https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md#time-entries)
+in Toggl board.
+
+
+
+
+#### SDK
+
+##### <a name="sdk"></a>sdk
+_Definition:_
+
+```ts
+(
+  options ?: object,
+) => object
+```
+
+_Usage:_
+
+```js
+const toggl = await togglConnector.sdk()
+```
+
+Get the underlying SDK object. You can specify additional options to override
+or add to the required fields in the connector's configuration.
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,399 @@
 import { Reshuffle, BaseConnector, EventConfiguration } from 'reshuffle-base-connector'
+import { CoreConnector, CoreEventHandler, Options } from './CoreConnector'
+import { nanoid } from 'nanoid'
+import { Moment } from 'moment'
+import pify from 'pify'
 
-export interface _CONNECTOR_NAME_ConnectorConfigOptions {
-  var1: string
-  // ...
+var TogglClient = require('toggl-api');
+
+interface EventOptions {
+  type: string
 }
 
-export interface _CONNECTOR_NAME_ConnectorEventOptions {
-  option1?: string
-  // ...
+export interface TogglConnectorConfigOptions {
+  token: string
 }
 
-export default class _CONNECTOR_NAME_Connector extends BaseConnector<
-  _CONNECTOR_NAME_ConnectorConfigOptions,
-  _CONNECTOR_NAME_ConnectorEventOptions
-> {
-  // Your class variables
-  var1: string
+export class TogglConnector extends CoreConnector {
 
-  constructor(app: Reshuffle, options?: _CONNECTOR_NAME_ConnectorConfigOptions, id?: string) {
+  private client: any
+  private token: string
+
+  constructor(app: Reshuffle, options: Options, id?: string) {
     super(app, options, id)
-    this.var1 = options?.var1 || 'initial value'
-    // ...
+    this.token = options.token
+    this.client = new TogglClient({ apiToken: this.token })
   }
 
-  onStart(): void {
-    // If you need to do something specific on start, otherwise remove this function
-  }
-
-  onStop(): void {
-    // If you need to do something specific on stop, otherwise remove this function
-  }
-
-  // Your events
-  on(
-    options: _CONNECTOR_NAME_ConnectorEventOptions,
-    handler: any,
-    eventId: string,
-  ): EventConfiguration {
-    if (!eventId) {
-      eventId = `_CONNECTOR_NAME_/${options.option1}/${this.id}`
+  // Events /////////////////////////////////////////////////////////
+  public on(
+    options: EventOptions,
+    handler: CoreEventHandler,
+    eventId?: string,
+  ) {
+    if (options.type !== 'TimeTrackerInitialized' &&
+      options.type !== 'TimeEntryAdded' &&
+      options.type !== 'TimeEntryModified' &&
+      options.type !== 'TimeEntryRemoved') {
+      throw new Error(`Invalid event type: ${options.type}`)
     }
-    const event = new EventConfiguration(eventId, this, options)
-    this.eventConfigurations[event.id] = event
 
-    this.app.when(event, handler)
-
-    return event
+    if (!eventId) {
+      eventId = `TOGGL/${this.id}/${eventId ? eventId : nanoid()}`
+    }
+    const eid = eventId
+    return this.eventManager.addEvent(options, handler, eid)
   }
 
-  // Your actions
-  action1(bar: string): void {
-    // Your implementation here
+  protected async onInterval() {
+    const [oldObjects, newObjects] = await this.store.update(
+      this.token,
+      () => this.getTimeEntriesRecords()
+    )
+
+    if (!oldObjects) {
+      await this.eventManager.fire(
+        (ec) => ec.options.type === 'TimeTrackerInitialized',
+        { objects: newObjects },
+      )
+      return
+    }
+
+    const diff = this.diffTimeEntries(oldObjects, newObjects)
+    if (0 < diff.changeCount) {
+      await this.eventManager.fire(
+        (ec) => ec.options.type === 'TimeEntryAdded',
+        diff.additions,
+      )
+      await this.eventManager.fire(
+        (ec) => ec.options.type === 'TimeEntryModified',
+        diff.modifications,
+      )
+      await this.eventManager.fire(
+        (ec) => ec.options.type === 'TimeEntryRemoved',
+        diff.removals,
+      )
+    }
   }
 
-  action2(foo: string): void {
-    // Your implementation here
+  private diffTimeEntries(oldObjects: TimeEntryRecord, newObjects: TimeEntryRecord) {
+    function likelyTheSameObject(o1: TimeEntry, o2: TimeEntry): boolean {
+      return (
+        o1.start === o2.start &&
+        o1.stop === o2.stop &&
+        o1.duration === o2.duration &&
+        o1.description === o2.description &&
+        JSON.stringify(o1.tags) == JSON.stringify(o2.tags)
+      )
+    }
+
+    const additions: TimeEntry[] = []
+    const modifications: TimeEntry[] = []
+    const removals: TimeEntry[] = []
+
+    for (const id in newObjects) {
+      if (id in oldObjects) {
+        if (!likelyTheSameObject(newObjects[id], oldObjects[id])) {
+          modifications.push(newObjects[id])
+        }
+      } else {
+        additions.push(newObjects[id])
+      }
+    }
+
+    for (const id in oldObjects) {
+      if (!(id in newObjects)) {
+        removals.push(oldObjects[id])
+      }
+    }
+
+    return {
+      changeCount: additions.length + modifications.length + removals.length,
+      additions,
+      modifications,
+      removals,
+    }
+  }
+
+  private async getTimeEntriesRecords() {
+    const res = await this.getTimeEntries()
+    if (!res) {
+      return {}
+    }
+
+    const objects: TimeEntryRecord = {}
+    res.forEach(function (te: TimeEntry) {
+      objects[te.id] = te
+    })
+    return objects
+  }
+
+  // Actions /////////////////////////////////////////////////////////
+
+  // Time Entry //
+
+  // If start_date and end_date are not specified, time entries started during the last 9 days are returned. 
+  // The limit of returned time entries is 1000.
+  async getTimeEntries(startDate?: string | number | Date | Moment, endDate?: string | number | Date | Moment) {
+    const getTimeEntries = pify(this.client.getTimeEntries.bind(this.client), { multiArgs: true })
+    const [data] = await getTimeEntries(startDate, endDate)
+    return data
+  }
+
+  async getCurrentTimeEntry() {
+    const getCurrentTimeEntry = pify(this.client.getCurrentTimeEntry.bind(this.client), { multiArgs: true })
+    const [data] = await getCurrentTimeEntry()
+    return data
+  }
+
+  async getTimeEntryData(teId: number | string) {
+    const getTimeEntryData = pify(this.client.getTimeEntryData.bind(this.client), { multiArgs: true })
+    const [data] = await getTimeEntryData(teId)
+    return data
+  }
+
+  async createTimeEntry(timeEntry: TimeEntry): Promise<any> {
+    const createTimeEntry = pify(this.client.createTimeEntry.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await createTimeEntry(timeEntry)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async startTimeEntry(timeEntry: TimeEntry) {
+    const startTimeEntry = pify(this.client.startTimeEntry.bind(this.client), { multiArgs: true })
+    const [data] = await startTimeEntry(timeEntry)
+    return data
+  }
+
+  async stopTimeEntry(teId: number | string) {
+    const stopTimeEntry = pify(this.client.stopTimeEntry.bind(this.client), { multiArgs: true })
+    const [data] = await stopTimeEntry(teId)
+    return data
+  }
+
+  async updateTimeEntry(teId: number | string, timeEntry: TimeEntry): Promise<any> {
+    const updateTimeEntry = pify(this.client.updateTimeEntry.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await updateTimeEntry(teId, timeEntry)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async updateTimeEntriesTags(teIds: number[] | string[], tags: string[], action: string): Promise<any> {
+    const updateTimeEntriesTags = pify(this.client.updateTimeEntriesTags.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await updateTimeEntriesTags(teIds, tags, action)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async deleteTimeEntry(teId: number | string): Promise<any> {
+    const deleteTimeEntry = pify(this.client.deleteTimeEntry.bind(this.client), { multiArgs: true })
+    const [data] = await deleteTimeEntry(teId)
+    return data
+  }
+
+  // Users //
+  async getUserData(user: User) {
+    const getUserData = pify(this.client.getUserData.bind(this.client), { multiArgs: true })
+    const [data] = await getUserData(user)
+    return data
+  }
+
+  async updateUserData(user: User): Promise<any> {
+    const updateUserData = pify(this.client.updateUserData.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await updateUserData(user)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async resetApiToken(): Promise<any> {
+    const resetApiToken = pify(this.client.resetApiToken.bind(this.client), { multiArgs: true })
+    const [data] = await resetApiToken()
+    return data
+  }
+
+  async changeUserPassword(currentPassword: string, password: string): Promise<any> {
+    const changeUserPassword = pify(this.client.changeUserPassword.bind(this.client), { multiArgs: true })
+    const [data] = await changeUserPassword(currentPassword, password)
+    return data
+  }
+
+  // TODO !! open PR in user.js -> change call to TogglClient.prototype.createUser !!
+  // async createUser(user: User): Promise<any> {
+  //   const createUser = pify(this.client.createUser.bind(this.client), { multiArgs: true })
+  //   const [data] = await createUser(user.email, user.password, user.timezone)
+  //   return data
+  // }
+
+  // Clients //
+  async createClient(clientData: ClientData): Promise<any> {
+    const createClient = pify(this.client.createClient.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await createClient(clientData)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async getClients() {
+    const getClients = pify(this.client.getClients.bind(this.client), { multiArgs: true })
+    const [data] = await getClients()
+    return data
+  }
+
+  async getClientData(clientId: number | string) {
+    const getClientData = pify(this.client.getClientData.bind(this.client), { multiArgs: true })
+    const [data] = await getClientData(clientId)
+    return data
+  }
+
+  async updateClient(clientId: number | string, clientData: ClientData) {
+    const updateClient = pify(this.client.updateClient.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await updateClient(clientId, clientData)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async deleteClient(clientId: number | string) {
+    const deleteClient = pify(this.client.deleteClient.bind(this.client), { multiArgs: true })
+    const [data] = await deleteClient(clientId)
+    return data
+  }
+
+  // active - Filter projects: active (true), archived (false), both
+  async getClientProjects(clientId: number | string, active: string | boolean) {
+    const getClientProjects = pify(this.client.getClientProjects.bind(this.client), { multiArgs: true })
+    const [data] = await getClientProjects(clientId, active)
+    return data
+  }
+
+  // Projects //
+  async createProject(projectData: ProjectData): Promise<any> {
+    const createProject = pify(this.client.createProject.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await createProject(projectData)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  async deleteProject(projectId: number | string) {
+    const deleteProject = pify(this.client.deleteProject.bind(this.client), { multiArgs: true })
+    const [data] = await deleteProject(projectId)
+    return data
+  }
+
+  async deleteProjects(projectIds: Array<number> | Array<string>) {
+    const deleteProjects = pify(this.client.deleteProjects.bind(this.client), { multiArgs: true })
+    const [data] = await deleteProjects(projectIds)
+    return data
+  }
+
+  async getProjectData(projectId: number | string) {
+    const getProjectData = pify(this.client.getProjectData.bind(this.client), { multiArgs: true })
+    const [data] = await getProjectData(projectId)
+    return data
+  }
+
+  async getProjectTasks(projectId: number | string) {
+    const getProjectTasks = pify(this.client.getProjectTasks.bind(this.client), { multiArgs: true })
+    const [data] = await getProjectTasks(projectId)
+    return data
+  }
+
+  async getProjectUsers(projectId: number | string) {
+    const getProjectUsers = pify(this.client.getProjectUsers.bind(this.client), { multiArgs: true })
+    const [data] = await getProjectUsers(projectId)
+    return data
+  }
+
+  async updateProject(projectId: number | string, projectData: ProjectData) {
+    const updateProject = pify(this.client.updateProject.bind(this.client), { multiArgs: true })
+    try {
+      const [data] = await updateProject(projectId, projectData)
+      return data
+    } catch (err) {
+      this.app.getLogger().error(err);
+    }
+  }
+
+  // SDK //
+  sdk(): any {
+    this.app.getLogger().info(`Toggl - SDK Returned`)
+    return this.client
   }
 }
 
-export { _CONNECTOR_NAME_Connector }
+export interface TimeEntry {
+  id: number,
+  wid: null | number,
+  pid: null | number,
+  billable: boolean,
+  start: string,
+  stop: string,
+  duration: number,
+  description: string,
+  tags: Array<string>,
+  at: string
+}
+
+type TimeEntryRecord = Record<number, TimeEntry>
+
+export interface User {
+  id: number,
+  email: string,
+  password: string,
+  timezone: string,
+  fullname: string,
+  send_product_emails: boolean
+  send_weekly_report: boolean
+  send_timer_notifications: boolean
+  store_start_and_stop_time: boolean
+  beginning_of_week: number // in the range of 0-6
+  timeofday_format: string, // Two formats are supported:
+  // "H:mm" for 24-hour format
+  // "h:mm A" for 12-hour format (AM/PM)
+  date_format: string // possible values: "YYYY-MM-DD", "DD.MM.YYYY", "DD-MM-YYYY", "MM/DD/YYYY", "DD/MM/YYYY", "MM-DD-YYYY"
+}
+
+export interface ClientData {
+  id: number,
+  name: string,
+  wid: null | number,
+  notes: string,
+  at: string
+}
+
+export interface ProjectData {
+  id: number,
+  name: string,
+  wid: null | number,
+  cid: null | number,
+  active: boolean,
+  is_private: boolean,
+  template: boolean,
+  template_id: number,
+  billable: boolean,
+  auto_estimates: boolean,
+  estimated_hours: number,
+  at: string,
+  color: string,
+  rate: number,
+  created_at: string
+}
